226bdfb39d2c7e84c4efa34d618e3c05f7b84280
JasonHonor <jasonhonor@outlook.com>
2017-09-04 11:06:49 +0800

    合并了star7th更新

---
+++ grant.sh
@@ -0,0 +1,7 @@
+chmod 777 ./Application/Runtime -R
+chmod 777 ./Application/Common/Conf/config.php -R
+chmod 777 ./Application/Home/Conf/config.php -R
+chmod 777 ./install -R
+chmod 777 ./server/Application/Runtime -R
+chmod 777 ./Public/Uploads/ -R
+chmod 777 ./Sqlite -R
